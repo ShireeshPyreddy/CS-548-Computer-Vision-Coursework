17a6ec0ef572b1284b23c515765a103d4e9184d2
Shireesh Reddy Pyreddy <pyredds@sunypoly.edu>
2023-12-05 14:47:24 -0500

    instead of efficient net, another simple model added

--- A05.py
+++ A05.py
@@ -5,12 +5,35 @@ from torchvision.transforms import v2
 import math
 
 
+class NaiveDeepNetwork(nn.Module):
+    def __init__(self, class_cnt):
+        super().__init__()
+        self.net_stack = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=(2, 2)),
+            nn.Tanh(),
+            
+            nn.Flatten(),
+            
+            nn.Linear(30752, 512),
+            nn.Tanh(),
+            nn.Linear(512, 128),
+            nn.Tanh(),
+            nn.Linear(128, 128),
+            nn.Tanh(),
+            nn.Linear(128, 128),
+            nn.Tanh(),
+            nn.Linear(128, 64),
+            nn.Tanh(),
+            nn.Linear(64, class_cnt),
+        )
+    
+    def forward(self, x):        
+        logits = self.net_stack(x)
+        return logits
+
 class SimpleNetwork(nn.Module):
     # Taken from: TorchLand.py, which was discussed in the class
-    """
-    APPROACH        TRAINING_accuracy       TRAINING_f1     TESTING_accuracy        TESTING_f1
-    simple_network  0.9512                  0.9512          0.7185                  0.7179
-    """
+    
     def __init__(self, class_cnt):
         super().__init__()        
         self.net_stack = nn.Sequential(
@@ -38,10 +61,7 @@ class SimpleNetwork(nn.Module):
         return logits
 
 class VanillaCNN(nn.Module):
-    """
-    APPROACH        TRAINING_accuracy       TRAINING_f1     TESTING_accuracy        TESTING_f1
-    vanilla_cnn     0.6851                  0.6814          0.6431                  0.6393
-    """
+
     def __init__(self, class_cnt):
         super().__init__()        
         self.net_stack = nn.Sequential(
@@ -55,7 +75,11 @@ class VanillaCNN(nn.Module):
             
             nn.Flatten(),
             
-            nn.Linear(1600, 32),
+            nn.Linear(1600, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
             nn.ReLU(),
             nn.Linear(32, class_cnt)
         )
@@ -65,10 +89,7 @@ class VanillaCNN(nn.Module):
         return logits
 
 class VanillaCNNWithDropOut(nn.Module):
-    """
-    APPROACH                        TRAINING_accuracy       TRAINING_f1     TESTING_accuracy        TESTING_f1
-    vanilla_cnn_with_dropout        0.6727                  0.6677          0.6496                  0.6443
-    """
+
     def __init__(self, class_cnt):
         super().__init__()   
         
@@ -86,10 +107,16 @@ class VanillaCNNWithDropOut(nn.Module):
             
             nn.Flatten(),
             
-            nn.Linear(1600, 32),
+            nn.Linear(1600, 512),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.Linear(32, class_cnt)
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(64, class_cnt)
         )
 
     def forward(self, x):        
@@ -97,10 +124,7 @@ class VanillaCNNWithDropOut(nn.Module):
         return logits
 
 class DeepCNN(nn.Module):
-    """
-    APPROACH        TRAINING_accuracy       TRAINING_f1     TESTING_accuracy        TESTING_f1
-    deep_cnn        0.9198                  0.9203          0.7903                  0.7919
-    """
+
     def __init__(self, class_cnt):
         super().__init__()
         
@@ -135,10 +159,7 @@ class DeepCNN(nn.Module):
         return logits
 
 class DeepCNNWithBatchNormDropOut(nn.Module):
-    """
-    APPROACH                            TRAINING_accuracy       TRAINING_f1     TESTING_accuracy        TESTING_f1
-    deep_cnn_with_batchnorm_dropout     0.8359                  0.8354          0.8036                  0.8027
-    """
+
     def __init__(self, class_cnt):
         super().__init__()
         
@@ -178,10 +199,7 @@ class DeepCNNWithBatchNormDropOut(nn.Module):
         return logits
 
 class VGG19Bn(nn.Module):
-    """
-    APPROACH        TRAINING_accuracy       TRAINING_f1     TESTING_accuracy        TESTING_f1
-    vgg19_bn        0.9981                  0.9981          0.9131                  0.9127
-    """
+
     # Taken from: https://github.com/chengyangfu/pytorch-vgg-cifar10/blob/master/vgg.py
     def __init__(self, features, class_cnt):
         super(VGG19Bn, self).__init__()
@@ -209,12 +227,8 @@ class VGG19Bn(nn.Module):
         return x
 
 class ResNet34(nn.Module):
-    """
-    APPROACH        TRAINING_accuracy       TRAINING_f1     TESTING_accuracy        TESTING_f1
-    resnet_34       0.9974                  0.9974          0.9641                  0.9641
-    """
-    
     # Modified the classification layer from VGG19Bn and desinged this classifier.
+    
     def __init__(self, features, class_cnt):
         super(ResNet34, self).__init__()
         self.features = features
@@ -256,15 +270,17 @@ class EfficientNetB7():
         return x
 
 def get_approach_names():
-    approaches = ["simple_network", "vanilla_cnn", "vanilla_cnn_with_dropout", "deep_cnn", "deep_cnn_with_batchnorm_dropout", "vgg19_bn", "resnet_34", "efficient_net_b7"]
+    approaches = ["naive_deep_network", "simple_network", "vanilla_cnn", "vanilla_cnn_with_dropout", "deep_cnn", "deep_cnn_with_batchnorm_dropout", "vgg19_bn", "resnet_34"]
     return approaches
 
 def get_approach_description(approach_name):
     description = ""
-    if approach_name == "simple_network":
-        description = ""
+    if approach_name == "naive_deep_network":
+        description = "It's a naive deep neural network that one convolutional layer followed by fully connected layers along with Tanh activation function"
+    elif approach_name == "simple_network":
+        description = "It's a simple CNN network taken from the class exercise."
     elif approach_name == "vanilla_cnn":
-        description = "It's a plain CNN model that has two convolutional layers."
+        description = "It's a plain CNN model that has two convolutional layers followed by multiple fully connected layers."
     elif approach_name == "vanilla_cnn_with_dropout":
         description = "It's a plain CNN model that has two convolutional layers with a dropout layer to avoid overfitting for each conv layer and for linear layer."
     elif approach_name == "deep_cnn":
@@ -272,11 +288,11 @@ def get_approach_description(approach_name):
     elif approach_name == "deep_cnn_with_batchnorm_dropout":
         description = "3 convolutional blocks with additional added layers like batch_norm and dropout to generalise the model and avoid overfitting"
     elif approach_name == "vgg19_bn":
-        description = "Fine tuning pre-trained VGG19 model with batch normalization."
+        description = "Fine tuning pre-trained VGG19 model with batch normalization and 300 epochs."
     elif approach_name == "resnet_34":
         description = "Fine tuning pre-trained ResNet 34 model by adding a custom classification layer with leakyrelu as activation function."
     elif approach_name == "efficient_net_b7":
-        description = "Fine tuning pre-trained EfficientNet b0 model by adding a custom classification layer with leakyrelu as activation function."
+        description = ""
     return description
 
 def get_data_transform(approach_name, training):
@@ -309,37 +325,23 @@ def get_data_transform(approach_name, training):
             data_transform.transforms.insert(1, v2.Resize((224, 224)))
             data_transform.transforms.insert(2, v2.Normalize(mean=[0.485, 0.456, 0.406],
                                                             std=[0.229, 0.224, 0.225]))
-            
-    elif approach_name in ['efficient_net_b7']:
-        if training is True:
-            data_transform.transforms.insert(1, v2.Resize((224, 224)))
-            # data_transform.transforms.insert(2, v2.AutoAugment(policy=v2.AutoAugmentPolicy.CIFAR10))
-            data_transform.transforms.insert(3, v2.Normalize(mean=[0.485, 0.456, 0.406],
-                                                            std=[0.229, 0.224, 0.225]))
-        
-        if training is False:
-            data_transform.transforms.insert(1, v2.Resize((224, 224)))
-            data_transform.transforms.insert(2, v2.Normalize(mean=[0.485, 0.456, 0.406],
-                                                            std=[0.229, 0.224, 0.225]))
 
     return data_transform
     
 def get_batch_size(approach_name):
-    if approach_name == "simple_network":
-      return 64  
-    elif approach_name == "vanilla_cnn":
+    if approach_name in ["vanilla_cnn", "naive_deep_network"]:
         return 32
-    elif approach_name == "vanilla_cnn_with_dropout":
-        return 64
-    elif approach_name == "deep_cnn":
-        return 64
-    elif approach_name == "deep_cnn_with_batchnorm_dropout":
-        return 64
+    elif approach_name in ["simple_network", "vanilla_cnn_with_dropout", "deep_cnn", "deep_cnn_with_batchnorm_dropout"]:
+        return 64  
     elif approach_name in ["vgg19_bn", "resnet_34", 'efficient_net_b7']:
         return 128
     
 def create_model(approach_name, class_cnt):
-    if approach_name == "simple_network":
+    if approach_name == "naive_deep_network":
+        naive_deep_network = NaiveDeepNetwork(class_cnt=class_cnt)
+        return naive_deep_network
+    
+    elif approach_name == "simple_network":
         simple_network = SimpleNetwork(class_cnt=class_cnt)  
         return simple_network
     
@@ -398,50 +400,24 @@ def create_model(approach_name, class_cnt):
         resnet_34 = ResNet34(features, class_cnt)
         return resnet_34
     
-    
-    elif approach_name == "efficient_net_b7":
-        
-        efficientnet = models.efficientnet_b0(pretrained=True)        
-        # efficientnet[1] = nn.Linear(efficientnet.in_features, class_cnt)
-        # efficientnet.classifier[1] = nn.Linear(efficientnet.classifier[1].in_features, class_cnt)
-        efficient_net = EfficientNetB7(efficientnet.classifier[1].in_features, class_cnt)
-
-        return efficient_net
-        
-        efficient_net_b7 = EfficientNetB7().efficient_net_b7(class_cnt)
-        
-        return efficient_net_b7
-    
 def train_model(approach_name, model, device, train_dataloader, test_dataloader):
     size = len(train_dataloader.dataset)
     loss_func = nn.CrossEntropyLoss()
-    
-    if approach_name == "simple_network":
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-        epochs = 25
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
         
-    if approach_name in ["vanilla_cnn", "vanilla_cnn_with_dropout"]:
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    if approach_name in ["naive_deep_network", "vanilla_cnn", "vanilla_cnn_with_dropout", "simple_network"]:
         epochs = 25
 
     elif approach_name in ["deep_cnn", "deep_cnn_with_batchnorm_dropout"]:
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
         epochs = 50   
     
     elif approach_name == "vgg19_bn":
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-        epochs = 20#300
+        epochs = 300
     
-    elif approach_name in ["resnet_34"]:
-        # params_1x = [param for name, param in model.named_parameters() if 'fc' not in str(name)]
+    elif approach_name == "resnet_34":
         lr, weight_decay = 1e-5, 5e-4
-        # optimizer = torch.optim.Adam([{'params':params_1x}, {'params': model.classifier.parameters(), 'lr': lr*10}], lr=lr, weight_decay=weight_decay)
         optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
         epochs = 10
-        
-    elif approach_name in ["efficient_net_b7"]:
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-        epochs = 5
     
     for each_epoch in range(epochs):
         print("##################### epoch", each_epoch, "#########################" )
